608fa83ab2f78511cc54801aa1bcd7fae1d7df54
anreh <anurehka@gmail.com>
2023-08-02 11:31:42 +0800

    Initial commit

--- app/src/main/java/com/example/practical3/MainActivity.kt
+++ app/src/main/java/com/example/practical3/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,12 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_four_text -> view.setBackgroundColor(Color.RED)
             R.id.box_five_text -> view.setBackgroundColor(Color.CYAN)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> findViewById<TextView>(R.id.box_three_text).setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> findViewById<TextView>(R.id.box_four_text).setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> findViewById<TextView>(R.id.box_five_text).setBackgroundResource(R.color.my_green)
+
             else -> view.setBackgroundColor(Color.DKGRAY)
         }
     }
@@ -35,6 +42,16 @@ class MainActivity : AppCompatActivity() {
 
         val clickableList: List<View> = listOf(boxOneText,boxTwoText,
             boxThreeText, boxFourText, boxFiveText, rootConstraintLayout)
+
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
+        val clickableViews: List<View> =
+            listOf(boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
+            )
 
         //boxOneText.setOnClickListener{makeColoured(it)
         //boxTwoText.setOnClickListener{makeColoured(it)
